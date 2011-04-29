62fc55b2afba50e08d80aa6a5c8a2d074f6e5fb4
Fredrik Karlsson <dargosch@users.sf.net>
2011-04-29 08:49:45 +0200

    Fix of HTML table issue, and made some changes to tye design:
    * Foreign keys are generated using dotted edges with "vee" arrow heads
    * Table name is in grey and spans the entire table
    * Borders are removed
    
    - Maybe indicies should be visualized too somehow, but I leave that for later.

--- sqlite2dot.tcl
+++ sqlite2dot.tcl
@@ -14,27 +14,26 @@ db eval {select name from sqlite_master where type = "table" and name NOT LIKE "
 
 }
 
-#puts "TABLES: $tables"
+
 
 append out  "digraph structs \{\n"
-#append out  "\trankdir=LR\n"
-append out  "\tnode \[shape=record,width=4\];\n"
+append out  "\tnode \[width=4,shape=plaintext\];\n"
 
 foreach currtab $tables {
 
 	set isFirst 1
 	set rows ""
 	db eval [format {PRAGMA table_info(%s);} $currtab] vals {
-		append rows [format {<TR><TD PORT="%s">%s</TD><TD>%s</TD></TR>} $vals(name) $vals(name)  $vals(type) ]
+		append rows [format {<TR><TD PORT="%s" ALIGN="LEFT">%s</TD><TD ALIGN="LEFT">%s</TD></TR>} $vals(name) $vals(name)  $vals(type)]
 		append rows "\n"
 	}
 
 
-	append out [format {			%s [label=<<TABLE PORT="%s"><TR><TD><B>%s</B></TD><TD></TD></TR>%s</TABLE>>];} $currtab $currtab $currtab $rows]
+	append out [format {			%s [label=<<TABLE PORT="%s" BORDER="0"><TR><TD BGCOLOR="grey" COLSPAN="2">%s</TD></TR>%s</TABLE>>];} $currtab $currtab $currtab $rows]
 	append out "\n"
 	
 	db eval [format {PRAGMA foreign_key_list(%s);} $currtab] vals {
-		append out [format {			%s:%s -> %s:%s;} $currtab $vals(from) $vals(table) $vals(to) ]
+		append out [format {			%s:%s -> %s:%s [arrowhead=vee,style=dotted];} $currtab $vals(from) $vals(table) $vals(to) ]
 		append out "\n"
 	}
 }
